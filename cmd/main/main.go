2170f220241afdf55612b50a0602b1216ec07b6b
Shreyank031 <shreyankchandu@gmail.com>
2024-05-05 20:14:02 +0530

    Add main.go and models

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"github.com/Shreyank031/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello")
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRouter(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9091", r))
 }
--- pkg/models/books.go
+++ pkg/models/books.go
@@ -1,1 +1,46 @@
 package models
+
+import (
+	"github.com/Shreyank031/go-bookstore/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:""json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+func (d *Book) CreateBook() *Book {
+	db.NewRecord(d)
+	db.Create(&d)
+
+	return d
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBookById(ID int64) Book {
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
+}
